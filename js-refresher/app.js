016ba219c8c393cc05aafba4c460e5ca69060a6c
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-24 19:19:10 +0530

    added basic js files

---
+++ js-refresher/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import { apiKey } from "./util.js";
+// alert("Hello, world!");
+console.log(`Api Key =  ${apiKey}`);
---
+++ js-refresher/util.js
@@ -0,0 +1,4 @@
+"use strict";
+
+let apiKey = "slkdjWlkslnc12lnlf$lndl^*#&#slknlnAncbf";
+export { apiKey };
