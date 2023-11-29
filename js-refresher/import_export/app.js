7e4ae23de3ee82b0a81c53b42304f8514969fcd4
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-29 17:32:25 +0530

    added short explanation for * in inport and export

--- js-refresher/import_export/app.js
+++ js-refresher/import_export/app.js
@@ -12,3 +12,7 @@ import defaultKey from "./util.js";
 // Printing the imported values.
 console.log(`Api Key =  ${apiKey}`);
 console.log(`Default Api Key = ${defaultKey}`); // default is only used when there is only 1 export required in that file.
+
+// The "*" keyword for import and export declarations -
+// Sometimes, we require to export multiple (more than 1) values/functions/operations etc to export at once and manually doing becomes hectic task so, we use "*" keyword for that.
+// For Example -
