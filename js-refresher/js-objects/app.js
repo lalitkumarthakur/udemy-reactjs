ef4337a75518b41512f76faac5f43983f18345b8
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-30 18:25:34 +0530

    added js objects defination

---
+++ js-refresher/js-objects/app.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// Objecta in Javascript.
+// An object is a collection of properties, and a property is an association between a name (or key) and a value. A property's value can be a function, in which case the property is known as a method. Objects in JavaScript, just as in many other programming languages, can be compared to objects in real life.
