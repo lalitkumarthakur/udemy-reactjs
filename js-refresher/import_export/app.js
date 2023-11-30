9c6ff8faec3e6b6895ae5dd761bacf1c2ae1cf29
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-30 16:41:11 +0530

    Added import and export concept with * operator

--- js-refresher/import_export/app.js
+++ js-refresher/import_export/app.js
@@ -16,3 +16,8 @@ console.log(`Default Api Key = ${defaultKey}`); // default is only used when the
 // The "*" keyword for import and export declarations -
 // Sometimes, we require to export multiple (more than 1) values/functions/operations etc to export at once and manually doing becomes hectic task so, we use "*" keyword for that.
 // For Example -
+
+// importing everything all at once but using "*".
+import * as myCalculator from "./file1.js";
+
+myCalculator.calculator();
