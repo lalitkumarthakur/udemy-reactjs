6debd1d778f5ba54555522fa00d31faf50bf8760
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-12-05 16:05:36 +0530

    repaired and removed unnessary CSS

--- react-js/create-react-app (old-react)/react-basics/src/index.js
+++ react-js/create-react-app (old-react)/react-basics/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "./index.css"; // This is global CSS file.
+import App from "./App"; // Importing Parent Component "App.js".
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<App /> {/* Declaring Parent component to use it. */}
+		{/* React Strict mode to detect error easily. */}
 	</React.StrictMode>
 );
