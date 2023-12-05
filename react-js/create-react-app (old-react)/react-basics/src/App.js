39a5d7454a8da40eaab487aa2c597ed4a03989ca
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-12-05 14:22:56 +0530

    Added both external and inline components

--- react-js/create-react-app (old-react)/react-basics/src/App.js
+++ react-js/create-react-app (old-react)/react-basics/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import InlineComp from "./components/External-components";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="MainBody">
+			<h1> Welcome to React Basic with OLD React.js (create-react-app)</h1>
+			<InlineComp />
+		</div>
+	);
 }
 
 export default App;
